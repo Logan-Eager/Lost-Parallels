5b39624b36b436fc761eefd93dc77f35fc42cae9
LoganEagerUc <lea46@uclive.ac.nz>
2024-09-26 19:23:47 +1200

    added interupt screen system
    akwardly embedded in movement script haha rip

--- lost parallels/Assets/Scripts/Player_movement.cs
+++ lost parallels/Assets/Scripts/Player_movement.cs
@@ -6,11 +6,12 @@ public class Player_movement : MonoBehaviour
 {
     public float moveSpeed;
     public float sprintSpeed;
+    public GameObject Interuptmenu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Interuptmenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,5 +22,10 @@ public class Player_movement : MonoBehaviour
         float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
 
         transform.position += moveDirection * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            {
+            Interuptmenu.SetActive(!Interuptmenu.activeSelf);
+        }
     }
 }
